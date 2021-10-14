6c7793724e700f354cf9ee5370ed1e5128d9b4c5
william <alown666@gmail.com>
2021-10-14 05:04:40 +0200

    adding isconnected

--- src/component/NavBar/index.jsx
+++ src/component/NavBar/index.jsx
@@ -1,29 +1,37 @@
-import logo from '../../img/argentBankLogo.png'
-import './style.css'
+import logo from '../../img/argentBankLogo.png';
+import './style.css';
+import { useSelector } from 'react-redux';
 
-
-function NavBar()
-{
+function NavBar() {
+    const isConnected = useSelector((state) => state.connected);
+    console.log(isConnected);
     return (
         <>
-        <nav className="main-nav">
-            <a className="main-nav-logo" href="/">
-            <img
-                className="main-nav-logo-image"
-                src={logo}
-                alt="Argent Bank Logo"
-            />
-            <h1 className="sr-only">Argent Bank</h1>
-            </a>
-            <div>
-            <a className="main-nav-item" href="/login">
-                <i className="fa fa-user-circle"></i>
-                &nbsp;Sign In
-            </a>
-            </div>
-        </nav>
-    </>)
+            <nav className="main-nav">
+                <a className="main-nav-logo" href="/">
+                    <img
+                        className="main-nav-logo-image"
+                        src={logo}
+                        alt="Argent Bank Logo"
+                    />
+                    <h1 className="sr-only">Argent Bank</h1>
+                </a>
+                <div>
+                    {isConnected ? (
+                        <a className="main-nav-item" href="/login">
+                            <i className="fa fa-user-circle"></i>
+                            &nbsp;Sign Out
+                        </a>
+                    ) : (
+                        <a className="main-nav-item" href="/login">
+                            <i className="fa fa-user-circle"></i>
+                            &nbsp;Sign In
+                        </a>
+                    )}
+                </div>
+            </nav>
+        </>
+    );
 }
 
-
-export default NavBar
+export default NavBar;
