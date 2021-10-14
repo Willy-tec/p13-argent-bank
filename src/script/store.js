8cc70466992dbfbcb9734986ec28c679ab5b6356
william <alown666@gmail.com>
2021-10-14 05:06:57 +0200

    adding some reducer function

--- src/script/store.js
+++ src/script/store.js
@@ -2,15 +2,38 @@ import { createStore } from 'redux';
 
 const initialState = {
     message: 'test',
+    token: 'nope',
+    connected: false,
+    firstName: '',
+    lastName: '',
 };
 
 function reducer(state = initialState, action) {
-    if (action.type === 'coucou')
-        return {
-            ...state,
-            message: 'coucou',
-        };
-    return state;
+    switch (action.type) {
+        case 'coucou':
+            return {
+                ...state, // TODO Make the initial redux state
+                message: 'coucou mon minou' + action.payload.txt,
+            };
+        case 'setToken':
+            return {
+                ...state,
+                token: action.payload.token,
+            };
+        case 'setConnect':
+            return {
+                ...state,
+                connected: action.payload.connected,
+            };
+        case 'setInfo':
+            return {
+                ...state,
+                firstName: action.payload.firstName,
+                lastName: action.payload.lastName,
+            };
+        default:
+            return state;
+    }
 }
 
 export const store = createStore(reducer);
